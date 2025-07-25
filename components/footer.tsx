01fc17b8337c9ddfb64ba524ab0ff99c8e92abd9
Hakdooooooooog <hicap.darenzjasper@gmail.com>
2025-07-25 10:37:21 +0800

    fix: optimize footer component by storing current year in a variable

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
   return (
     <footer className="w-full fixed z-999 bottom-0 text-center p-4 bg-gray-100 dark:bg-gray-800">
       <p className="text-gray-600 dark:text-gray-300">
-        © {new Date().getFullYear()} Hicap&apos;s Portfolio. All rights
-        reserved.
+        © {currentYear} Hicap&apos;s Portfolio. All rights reserved.
       </p>
     </footer>
   );
